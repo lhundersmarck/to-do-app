f22bca04c0271846a30bbfbad32e4537b193cf85
lhundersmarck <laurahundersmarck@gmail.com>
2019-05-12 10:38:07 -0400

    Create Event Handler

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+function onReady() {
+  const addToDoForm = document.getElementById('addToDoForm');
+  const newToDoText = document.getElementById('newToDoText');
+  const toDoList = document.getElementById('toDoList');
+}
+
 window.onload = function() {
-    alert("The window has loaded!");
+    onReady();
 };
